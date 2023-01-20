585ad56ad95c37bd6bba5628edd49f8737d71221
Marcin <marcin.9302@gmail.com>
2023-01-20 07:57:01 +0100

    change header content

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ const closeButton = document.querySelector('.fa-circle-xmark');
 // }
 
 function showMap() {
-    // mapSection.style.display = "block";
-    // header.classList.add('blur');
-    // section.classList.add('blur');
-    // document.body.classList.add('overflowHidden');
+    mapSection.style.display = "block";
+    header.classList.add('blur');
+    section.classList.add('blur');
+    document.body.classList.add('overflowHidden');
     console.log('test')
 }
 
 function closeMap() {
-    // mapSection.style.display = "none";
-    // header.classList.remove('blur');
-    // section.classList.remove('blur');
-    // document.body.classList.remove('overflowHidden');
+    mapSection.style.display = "none";
+    header.classList.remove('blur');
+    section.classList.remove('blur');
+    document.body.classList.remove('overflowHidden');
 }
 
 
 // navbar.addEventListener('mouseover', navHover)
 mapButton.addEventListener('click', showMap);
-// closeButton.addEventListener('click', closeMap);
+closeButton.addEventListener('click', closeMap);
